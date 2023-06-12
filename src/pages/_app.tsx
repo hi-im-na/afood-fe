91378bd16a4159e4f64716a1f8030d7e9ebe96e9
Na <lengocanh2k@gmail.com>
2023-06-12 11:29:09 +0700

    config ts, mui, prettier, eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import Layout from '@/components/layout/layout'
+import { AuthProvider, ProtectedRoute } from '@/context/auth'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthProvider>
+        {/* <ProtectedRoute> */}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        {/* </ProtectedRoute> */}
+      </AuthProvider>
+    </>
+  )
 }
