bac4603c1ece5da49641ec0b48be8269ea069611
Na <lengocanh2k@gmail.com>
2023-06-27 18:04:51 +0700

    add variables

---
+++ src/utils/globalVariables.ts
@@ -0,0 +1,2 @@
+export const headerHeight = 64
+export const headerTotalHeight = 104
--- src/utils/months.ts
+++ src/utils/months.ts
@@ -13,6 +13,58 @@ const MONTHS = [
   'December',
 ]
 
+export const monthsValues = [
+  {
+    value: 1,
+    label: 'January',
+  },
+  {
+    value: 2,
+    label: 'February',
+  },
+  {
+    value: 3,
+    label: 'March',
+  },
+  {
+    value: 4,
+    label: 'April',
+  },
+  {
+    value: 5,
+    label: 'May',
+  },
+  {
+    value: 6,
+    label: 'June',
+  },
+  {
+    value: 7,
+    label: 'July',
+  },
+  {
+    value: 8,
+    label: 'August',
+  },
+  {
+    value: 9,
+    label: 'September',
+  },
+  {
+    value: 10,
+    label: 'October',
+  },
+  {
+    value: 11,
+
+    label: 'November',
+  },
+  {
+    value: 12,
+    label: 'December',
+  },
+]
+
 const months = (config: any) => {
   const cfg = config || {}
   const count = cfg.count || 12
