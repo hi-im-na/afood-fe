cd098832221e764a4b99a5b7bf9e87b5b1ca69aa
Na <lengocanh2k@gmail.com>
2023-06-29 14:36:28 +0700

    update menu UI

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,38 +1,111 @@
-import Menus from '@/components/menus/Menus'
-import { IMenu } from '@/models/models'
-import { fetchMenus } from '@/services/publicApi'
-import { Box, Typography } from '@mui/material'
+import { IFood, IMenu } from '@/models/models'
+import { fetchFoods, fetchFoodsByMenuId, fetchMenus } from '@/services/publicApi'
+import FoodInMenu from '@/components/menus/FoodInMenu'
+import { Box, Tab, Tabs, Typography } from '@mui/material'
+import { tabsClasses } from '@mui/material/Tabs'
 import { GetServerSideProps } from 'next'
+import { useState } from 'react'
+import { BrunchDining, Icecream, KebabDining, LocalBar, LocalDining, LocalPizza } from '@mui/icons-material'
 
 interface MenuProps {
   menus: IMenu[]
+  foods: IFood[]
+  foodsInMenus: IFood[][]
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const menus = await fetchMenus()
+  const menus: IMenu[] = await fetchMenus()
+  const foods: IFood[] = await fetchFoods()
 
-  return { props: { menus } }
+  const foodsInMenus: IFood[][] = await Promise.all(
+    menus.map(async (menu) => {
+      const foodsInMenu = await fetchFoodsByMenuId(menu.id)
+      return foodsInMenu
+    })
+  )
+
+  return { props: { menus, foods, foodsInMenus } }
 }
 
-const MenuPage = ({ menus }: MenuProps) => {
-  // const Menu = () => {
-  // const { data: menus, error } = useSWR('/api/menus', fetchMenus)
-  // if (error) return <div>failed to load</div>
-  // if (!menus) return <div>loading...</div>
-  // return <div>hello {menus}!</div>
-  // console.log(menus)
+const MenuPage = ({ menus, foods, foodsInMenus }: MenuProps) => {
+  const [value, setValue] = useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
+  const iconList = [
+    <KebabDining />,
+    <BrunchDining />,
+    <Icecream />,
+    <LocalBar />,
+    <LocalPizza />,
+  ]
+
   return (
     <Box>
-      <Typography
-        variant="h2"
-        textAlign="center"
+      <Typography variant="h3" textAlign="center" sx={{ pb: 4 }} color={'primary'}>
+        Menu
+      </Typography>
+      <Box
         sx={{
-          my: 2,
+          flexGrow: 1,
+          maxWidth: { xs: 320, sm: 480 },
+          bgcolor: 'background.paper',
+          margin: 'auto',
         }}
       >
-        Menus
-      </Typography>
-      <Menus menus={menus}></Menus>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons
+          aria-label="visible arrows tabs example"
+          sx={{
+            [`& .${tabsClasses.scrollButtons}`]: {
+              '&.Mui-disabled': { opacity: 0.3 },
+            },
+          }}
+          indicatorColor='secondary'
+        >
+          <Tab label="All food" icon={<LocalDining />} />
+          {menus.map((menu) => (
+            <Tab label={menu.name} key={menu.id} icon={iconList[menu.id - 1]} />
+          ))}
+        </Tabs>
+      </Box>
+      <Box
+        sx={{
+          display: value === 0 ? 'flex' : 'none',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          alignItems: 'stretch',
+          gridAutoRows: '1fr',
+        }}
+      >
+        {foods.map((food) => (
+          <FoodInMenu foodInMenu={food} key={food.id} />
+        ))}
+      </Box>
+      {menus.map((menu, index) => (
+        <Box
+          key={menu.id}
+          sx={{
+            display: value === index + 1 ? 'flex' : 'none',
+            flexWrap: 'wrap',
+            alignItems: 'stretch',
+            // gridAutoRows: '1fr',
+          }}
+        >
+          {foodsInMenus[index].map((food) => {
+            return (
+              <>
+                <FoodInMenu foodInMenu={food} key={food.id} />
+              </>
+            )
+          })}
+        </Box>
+      ))}
     </Box>
   )
 }
